df54f90631b233f0cb088090333f282e90b32bb8
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2024-08-17 17:55:42 +0500

    feat: application routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./ui/Navbar";
+import Home from "./ui/Home";
+import ProductsItemDetail from "./features/products/ProductsItemDetail";
+import Basket from "./features/basket/Basket";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/products/:id" exact element={<ProductsItemDetail />} />
+          <Route path="/basket" exact element={<Basket />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
