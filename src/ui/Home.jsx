67427176ab9f18fa9a7591403e9dda50c89162ad
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2024-08-17 17:59:04 +0500

    feat: Home component

---
+++ src/ui/Home.jsx
@@ -0,0 +1,13 @@
+import Products from "../features/products/Products";
+
+function Home() {
+  return (
+    <>
+      <main className="py-2 px-2 md:px-3 md:py-3">
+        <Products />
+      </main>
+    </>
+  );
+}
+
+export default Home;
