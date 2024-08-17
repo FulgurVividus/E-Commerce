26779c764da52cece17ff4dcae481f01cc6092cb
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2024-08-17 21:23:28 +0500

    styles

--- src/common/Loader.jsx
+++ src/common/Loader.jsx
@@ -1,6 +1,6 @@
 function Loader() {
   return (
-    <div className="absolute inset-0 z-50 flex h-full items-center justify-center bg-gray-500/20 backdrop-blur-lg">
+    <div className="absolute mx-auto inset-0 z-50 flex h-full items-center justify-center bg-gray-500/20 backdrop-blur-sm">
       <div className="loader"></div>
     </div>
   );
--- src/features/basket/Basket.jsx
+++ src/features/basket/Basket.jsx
@@ -1,5 +1,9 @@
 function Basket() {
-  return <div>Basket</div>;
+  return (
+    <>
+      <div className="py-2 px-2 md:px-3">Basket</div>
+    </>
+  );
 }
 
 export default Basket;
