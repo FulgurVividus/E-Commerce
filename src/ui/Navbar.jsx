2d2117d4c9ad083ad19a59dd0f4abc8ec5d86142
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2024-08-17 17:57:42 +0500

    feat: Navbar (not full functionality)

---
+++ src/ui/Navbar.jsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <>
+      <nav>
+        <ul className="flex justify-between items-center py-2 px-2 md:px-3 bg-gray-100">
+          <div>
+            <li className="font-bold text-lg md:text-xl">
+              <Link to="/">E-Commerce</Link>
+            </li>
+          </div>
+
+          <div className="flex items-center gap-3">
+            <li>
+              <input
+                type="search"
+                placeholder="Search..."
+                className="border-2 border-gray-400 rounded-2xl py-1 px-1 md:px-2 focus:outline-none focus:ring focus:ring-gray-500 focus:ring-opacity-50  text-black duration-300"
+              />
+            </li>
+            <li className="font-semibold text-sm md:text-base">
+              <Link to="/basket">Basket</Link>
+            </li>
+          </div>
+        </ul>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
